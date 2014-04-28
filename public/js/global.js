95ae712b7bfd729e539862574d429034ee337c8a
ilpaijin <ilpaijin@gmail.com>
2014-04-28 19:21:54 +0200

    Background Image / socket.io

---
+++ public/js/global.js
@@ -0,0 +1,11 @@
+var yir = yir || {};
+
+yir.Backgrounder = {
+    imgsPrefix: "amsterdam-",
+    imgsCount: 4
+};
+
+yir.Backgrounder.choosenOne = Math.floor((Math.random() * yir.Backgrounder.imgsCount) + 1);
+yir.Backgrounder.imgUrl = "public/img/" + yir.Backgrounder.imgsPrefix + yir.Backgrounder.choosenOne;
+
+document.getElementsByTagName("html")[0].style.background = 'url("' + yir.Backgrounder.imgUrl + '.jpg") no-repeat center center fixed';
