829daf7dbf88b774f8b9f5f1f12398e79ce4fb40
ilpaijin <ilpaijin@gmail.com>
2014-05-01 20:33:33 +0200

    amazon AWS stuff

--- public/js/global.js
+++ public/js/global.js
@@ -1,7 +1,10 @@
 var yir = yir || {};
 
 yir.config = {
-    NodeAppServer: 'http://192.168.178.15:3001/broadcast',
+    NodeAppServer: {
+        "localhost": 'http://192.168.178.15:3001/broadcast',
+        "54.72.77.96": 'http://54.72.77.96:3001/broadcast' // amazon AWS
+    },
     GoAppServer: 'http://localhost:3000/echo'
 }
 
